9feb7812f6c6f3e00d0bddfba3159b1938631f5e
sdce.gaoxy@gmail.com <sdce.gaoxy@gmail.com>
2016-01-07 20:48:18 -0800

    fix carousels

--- JsPlugins/js/gCarousels.js
+++ JsPlugins/js/gCarousels.js
@@ -14,74 +14,80 @@
 		}, options );
 		
         
-		var pwidth=$("#gcarousels").width();
-		var pheight=$("#gcarousels").height();
+		var pwidth=$(this).width();
+		var pheight=$(this).height();
 		var img_width=pwidth*parseFloat(settings.width)/100;
 		var img_height=pheight*parseFloat(settings.height)/100;
 		
 		
-//		console.log(imageCarousels);
-//		console.log("#gcarousels width="+img_width);
-//		console.log("#gcarousels height="+img_height);
-//		
-        $("#gcarousels").html(getCarouselsHtml(settings,img_width,img_height));
+        $(this).html(getCarouselsHtml(settings,img_width,img_height));
 		
 		var topmargin=img_height-parseFloat(settings.descheight)*16;
 		console.log("img_height="+img_height);
 		console.log("settings.deschegiht="+settings.descheight);
 		console.log("topmargin="+topmargin);
+ 
+		setDefaultCSS(this,settings,img_width,img_height,topmargin);
+		initlizeMouseEvent(this);
+		running(this,img_width);
 		
-		$("#gcarousels").css("overflow","hidden");
-		$("#gcarousels,ul,li,img").css("margin","0px"); 
-		$("#gcarousels,ul,li,img").css("padding","0px"); 
-		$("#gcarousels li").css("list-style","none");
-		$("#gcarousels li").css("float","left");
-		$("#gcarousels ul").css("width","1000px");
-		$("#gcarousels li").css("width",img_width);
-		$("#gcarousels li").css("height",img_height);
-		
-		$("#gcarousels span").css("text-align","center");
-		$("#gcarousels span").css("width",img_width);
-		$("#gcarousels span").css("height",settings.descheight);
-        $("#gcarousels span").css("margin-top",topmargin);
-		 $("#gcarousels span").css("padding-top",parseFloat(settings.descheight)/4+"em");
-		$("#gcarousels span>a:nth-child(1)").css("float","left");
-		$("#gcarousels span>a:nth-child(2)").css("float","right");
-		
-		
-		
-		$("#gcarousels span").css("background-color","gray");
-		$("#gcarousels span").css("opacity","0.6");
-		$("#gcarousels span").css("filter","alpha(opacity=60)");/* For IE8 and earlier */
-		
-		
-		running(img_width);
-		initlizeMouseEvent();
    
 		return this;
     }
 }(jQuery));
 
+function setDefaultCSS(obj,settings,img_width,img_height,topmargin){
 
+	
+	obj.css("overflow","hidden");
+	obj.children("ul").css({
+		margin:"0px",
+		padding:"0px",
+		"width":"1000px"
+	});
+	
+	obj.children("ul").children("li").css({
+		margin:"0px",
+		padding:"0px",
+		"list-style":"none",
+		"float":"left",
+		"width":img_width,
+		"height":img_height
+	});
+ 	
+	obj.children("ul").children("li").children("span").css({
+		"text-align":"center",
+		"width":img_width,
+		"height":settings.descheight,
+		"margin-top":topmargin,
+		"padding-top":parseFloat(settings.descheight)/4+"em",
+		"background-color":"gray",
+		"opacity":"0.6",
+		"filter":"alpha(opacity=60)"
+	});
+	
+	obj.children("ul").children("li").children("span").children("a:nth-child(1)").css("float","left");
+	obj.children("ul").children("li").children("span").children("a:nth-child(3)").css("float","right");
+}
 
 function getCarouselsHtml(data,img_width,img_height){
 	var html="";
 	$.each( data.datasource, function( i,obj ) {
-   		html=html+"<li style=\"background-image:url("+obj.imageUrl+")\"><a target=\"_blank\" href=\""+obj.targeturl+"\">"+
+   		html=html+"<li style=\"background-image:url("+obj.imageUrl+")\">"+
 //				  "<img src=\""+obj.imageUrl+"\" width=\""+
 //					img_width+"\" height=\""+img_height+"\"/>"+
-				  "</a><span style=\"display:none\">"+
+				  "<span style=\"display:none\">"+
 				  "<a href=\"#\">&lt;&lt;Prev</a>"+
-					obj.desc+
+				  "<a target=\"_blank\" href=\""+obj.targeturl+"\">"+obj.desc+"</a>"+
 					"<a href=\"#\">Next&gt;&gt;</a></span></li>";
 	});
-	html="<ul>"+html+"</ul>"
-	html=html+"<div id=\"gcarousels_nav\"><div>"
+	html="<ul>"+html+"</ul>";
+	
 	return html;
 }
 
-function running(width){
-	var $ul=$("#gcarousels>ul");
+function running(obj,width){
+	var $ul=obj.children("ul");
 	setInterval(function(){  
                     $ul.animate({  
                             'margin-left':'-'+ width+'px'  
@@ -99,46 +105,44 @@ function running(width){
      );  
 }
 
+
+
 function caculateHegiht(settings){
 	var objlen=settings.datasource.length;
 	var boxheight=settings.height;
 	var lineheight
 }
 
-function initlizeMouseEvent(){
+function initlizeMouseEvent(obj){
 	//on mouse over ul show desc
 	//on mouse over ul show nav button
 	//response mouse click nav event
-
-	
-	$( "#gcarousels" ).bind({
+	obj.bind({
 		  mouseover: function() {
-			$("#gcarousels span").show();
-			$("#gcarousels span").css("display","block");
-			  
+			  obj.children("ul").children("li").children("span").css("display","block");
 		  },
 		  mouseout: function() {
-			$("#gcarousels span").hide();
+			obj.children("ul").children("li").children("span").hide();
 	}});
 	
 	
-	$( "#gcarousels span>a:nth-child(1)" ).bind({
+	obj.children("ul").children("li").children("span").children("a:nth-child(1)").bind({
 		  click: function() {
 			 
-			  $("#gcarousels>ul").css({'margin-left':0}).  
+			  obj.children("ul").css({'margin-left':0}).  
                                 children('li').  
                                 first().  
-                                before(                              														$("#gcarousels>ul").children('li').last()  
+                                before(                              														obj.children("ul").children('li').last()  
                                 ); 
 			  
 		  }});
 	
-		$( "#gcarousels span>a:nth-child(2)" ).bind({
+		obj.children("ul").children("li").children("span").children("a:nth-child(3)").bind({
 		  click: function() {
-			$("#gcarousels>ul").css({'margin-left':0}).  
+			obj.children("ul").css({'margin-left':0}).  
                                 children('li').  
                                 last().  
-                                after(                              														$("#gcarousels>ul").children('li').first()  
+                                after(                              														obj.children("ul").children('li').first()  
                                 );  
 		  }});
 }
